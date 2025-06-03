45559ee95764eb991f9399507d9f21ced1e47065
viswanthswarna <viswanthswarna184@gmail.com>
2025-06-03 20:01:59 +0530

    'made_it_more_flexible'

--- sleep_sound.bat
+++ sleep_sound.bat
@@ -1,21 +1,34 @@
-@echo off
 
-:: Delay until 11:00 PM
+@echo off
+setlocal
+
+:: Target time: 23:00 (11:00 PM) in 24-hour format
+set targetHour=23
+set targetMinute=00
+
 :waitForTime
-for /f %%t in ('time /t') do set currTime=%%t
-if "%currTime%"=="11:00 PM" goto play
-timeout /t 60 >nul
+for /f "tokens=1-2 delims=:" %%a in ("%time%") do (
+    set /a currHour=1%%a-100
+    set /a currMinute=1%%b-100
+)
+
+if %currHour% GTR %targetHour% goto play
+if %currHour% EQU %targetHour% if %currMinute% GEQ %targetMinute% goto play
+
+timeout /t 30 >nul
 goto waitForTime
 
 :play
-:: Start playing audio file with VLC (adjust the path)
-start "" "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\sai\Documents\sleep\ocean-beach-waves.mp3"
+:: Start VLC audio playback (adjust path as needed)
+start "" "C:\Program Files\VideoLAN\VLC\vlc.exe" "C:\Users\sai\Documents\sleep\ocean-beach-waves.mp3" --loop
 
-:: Wait for 90 minutes (from 11:00 PM to 12:30 AM = 5400 seconds)
+nircmd monitor off
+
+:: Wait 90 minutes (from 11:00 PM to 12:30 AM)
 timeout /t 5400 /nobreak
 
 :: Kill VLC player
 taskkill /IM vlc.exe /F
 
-:: Put the computer to sleep
+:: Put the PC to sleep
 rundll32.exe powrprof.dll,SetSuspendState 0,1,0
